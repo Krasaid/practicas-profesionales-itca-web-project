3725adef4bc0e3f0c361e82f1d450d72f0b3e712
Krasaid <fernandomartinezg1d@gmail.com>
2025-10-24 11:49:46 -0600

    pruebas en postman y configuracion de mysql

--- src/main/java/sv/edu/itca/practicas_profesionales_itca_web/model/EstadoPropuesta.java
+++ src/main/java/sv/edu/itca/practicas_profesionales_itca_web/model/EstadoPropuesta.java
@@ -1,3 +1,4 @@
+//EstadoPropuesta enum
 package sv.edu.itca.practicas_profesionales_itca_web.model;
 
 public enum EstadoPropuesta {
--- src/main/java/sv/edu/itca/practicas_profesionales_itca_web/service/AlumnoService.java
+++ src/main/java/sv/edu/itca/practicas_profesionales_itca_web/service/AlumnoService.java
@@ -1,4 +1,4 @@
-package sv.edu.itca.practicas_profesionales_itca_web.service;
+package sv.edu.itca.practicas_profesionales_itca_web.service; // (Asegúrate que el paquete sea el tuyo)
 
 import sv.edu.itca.practicas_profesionales_itca_web.model.EstadoPropuesta;
 import sv.edu.itca.practicas_profesionales_itca_web.model.Propuesta;
@@ -6,11 +6,10 @@ import sv.edu.itca.practicas_profesionales_itca_web.model.Usuario;
 import sv.edu.itca.practicas_profesionales_itca_web.repository.PropuestaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDate;
 import java.util.List;
 
-@Service // <-- Esta anotación es la que arregla el "No beans found"
+@Service
 public class AlumnoService {
 
     private final PropuestaRepository propuestaRepo;
@@ -21,20 +20,21 @@ public class AlumnoService {
     }
 
     public void submitPropuesta(Propuesta nuevaPropuesta, Usuario alumno) {
-
         boolean tieneActiva = propuestaRepo.existsByAlumnoAndEstado(alumno, EstadoPropuesta.EN_REVISION);
         if (tieneActiva) {
             throw new IllegalStateException("Ya tienes una propuesta en revisión.");
         }
-
         int horasAcumuladas = getHorasAcumuladas(alumno);
         if (horasAcumuladas >= 320) {
             throw new IllegalStateException("Felicidades, ya completaste tus 320 horas.");
         }
-
         nuevaPropuesta.setAlumno(alumno);
         nuevaPropuesta.setEstado(EstadoPropuesta.EN_REVISION);
-        nuevaPropuesta.setFechaEnvio(LocalDate.now());
+        nuevaPropuesta.setFechaEnvio(LocalDate.now()); // ¡Fix de fecha!
+        // Añadimos los campos que faltaban del formulario de React
+        nuevaPropuesta.setDescripcion(nuevaPropuesta.getDescripcion());
+        nuevaPropuesta.setFechaInicio(nuevaPropuesta.getFechaInicio());
+        nuevaPropuesta.setFechaFin(nuevaPropuesta.getFechaFin());
 
         propuestaRepo.save(nuevaPropuesta);
     }
